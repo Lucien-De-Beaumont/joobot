cb26abcbd71904ea51671b8ac454643113a23e54
Lucien-De-Beaumont <raphael50110@yahoo.fr>
2022-08-05 02:41:50 +0200

    added restart.js

---
+++ commands/restart.js
@@ -0,0 +1,14 @@
+const config = require('../config');
+
+module.exports = {
+    name: "setslash",
+    description: "Actualisation des commandes",
+    role: [config.dev['Mécabot']],
+    hidden: true,
+    helpType: "dev",
+
+    runInteraction(client, interaction) {
+        interaction.reply(`Redémarrage du bot en cours..`)
+        setTimeout(() => { process.exit(); }, 200);
+    }
+}
