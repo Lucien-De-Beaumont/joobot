b8e776283c644b7d655c74f0e6ce97479e25ea8e
Lucien-De-Beaumont <raphael50110@yahoo.fr>
2022-08-09 22:36:50 +0200

    added register.js

---
+++ commands/register.js
@@ -0,0 +1,71 @@
+const Discord = require("discord.js");
+const config = require("../config");
+const db = require("../utils/connectMYSQL");
+
+module.exports = {
+    name: "register",
+    description: "Créer un nouveau personnage RP",
+    role: [config.guild],
+    hidden: false,
+    helpType: "fun",
+    underConstruction: false,
+    options: [{
+        name: "nom",
+        description: "Quel sera le nom de votre personnage RP ?",
+        required: true,
+        type: "STRING"
+    }, {
+        name: "prefixe",
+        description: "Par quelle portion de texte devra commencer votre message pour que votre personnage parle ?",
+        required: true,
+        type: "STRING"
+    }, {
+        name: "avatar",
+        description: "Quel sera le lien de l'avatar de votre personnage ?",
+        required: true,
+        type: "STRING"
+    }
+    ],
+
+    async runInteraction(client, interaction) {
+        let nom = interaction.options.getString('nom')
+        let prefix = interaction.options.getString('prefixe')
+        let avatarURL = interaction.options.getString('avatar')
+
+        function isImage(url) {
+            return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+        }
+
+        if (!isImage(avatarURL)) { return interaction.reply({ content: `Oops! Ce lien ne semble pas rediriger vers une image!`, ephemeral: true }) }
+        const button = new Discord.MessageActionRow()
+            .addComponents(
+                new Discord.MessageButton()
+                    .setCustomId('validate-character-button')
+                    .setLabel('✅ | Créer mon personnage')
+                    .setStyle('SUCCESS'),
+                new Discord.MessageButton()
+                    .setCustomId('refuse-character-button')
+                    .setLabel('❌ | Revenir en arrière')
+                    .setStyle('DANGER'),
+            )
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`Validation de création d'un nouveau personnage`)
+            .setAuthor({ name: interaction.member.displayName, iconURL: interaction.member.displayAvatarURL() })
+            .setThumbnail(`${avatarURL}`)
+            .setDescription(`Merci de vérifier les informations fournies, les seules modifications possibles se restreignent à changer l'avatar de votre personnage.\nSi l'avatar n'apparaît pas dans le message, merci de considérer que celui n'est pas valide.\nEn conséquent, toute autre information est figée définitivement ( quoique modifiable par le <@&${config.dev['Mécano']}>, <@553231950958035004> ).`)
+            .addFields({
+                name: `Nom du personnage`, value: `${nom}`, inline: true
+            }, {
+                name: `Préfixe`, value: `${prefix}`, inline: true
+            }, {
+                name: `Lien de l'avatar`, value: `${avatarURL}`, inline: true
+            }, {
+                name: `Exemple type d'utilisation`, value: `\`\`\`${prefix} Bonjour ! Je suis ${nom} !\`\`\``, inline: false
+            }, {
+                name: `Retour du message`, value: `\`\`\`Bonjour ! Je suis ${nom} !\`\`\``, inline: false
+            })
+            .setTimestamp()
+
+        interaction.reply({ embeds: [embed], components: [button] })
+    },
+}
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,10 +4,34 @@ const Discord = require("discord.js");
 module.exports = {
   name: "messageCreate",
   once: false,
-  execute(client, message) {
+  async execute(client, message) {
     if (message.author.bot) return;
-
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+
+    let prefix = 'llm!'
+    if (message.content.startsWith(prefix)) {
+      // let avatarURL = args[2]
+      // let username = args[3]
+
+      let content = args.slice(0).join(" ").replace(prefix);
+
+      const webhooks = await message.guild.fetchWebhooks();
+      const webhook = webhooks.find(wh => wh.token);
+
+      await webhook.edit({
+        channel: message.channel.id
+      });
+
+      message.delete()
+
+      webhook.send({
+        content: content,
+        username: 'Lucien le mécano',
+        avatarURL: 'https://fr.web.img5.acsta.net/c_310_420/medias/nmedia/18/69/18/59/18865095.jpg',
+      });
+
+    }
+
     if (message.content.slice(0, config.prefix.length) !== config.prefix) return;
     const cmdName = args.shift().toLowerCase();
     if (!cmdName.length) return;
