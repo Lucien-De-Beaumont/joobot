61f2d230c1284386e551463f00bfce6a716a8a07
Lucien-De-Beaumont <raphael50110@yahoo.fr>
2022-08-09 17:43:13 +0200

    fixed some things

--- commands/tupper.js
+++ commands/tupper.js
@@ -3,15 +3,20 @@ const config = require("../config");
 const db = require("../utils/connectMYSQL");
 
 module.exports = {
-    name: "webhook",
+    name: "tupper",
     description: "Commandes test webhook",
-    role: [config.moderation['Fondateur'], config.moderation['Administrateur']],
+    role: [config.guild],
     hidden: false,
     helpType: "fun",
     underConstruction: true,
 
     run(client, message) {
-
+        message.channel.createWebhook({
+            name: 'Some-username',
+            avatar: 'https://i.imgur.com/AfFp7pu.png',
+        })
+            .then(webhook => console.log(`Created webhook ${webhook}`))
+            .catch(console.error);
     },
 
     runInteraction(client, interaction) {
