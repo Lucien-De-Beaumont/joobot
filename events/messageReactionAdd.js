db1252cd439f0ff2d512f0dc8282f6eaf56265cd
Lucien-De-Beaumont <raphael50110@yahoo.fr>
2022-08-18 02:59:42 +0200

    started editing functionality

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -12,6 +12,25 @@ module.exports = {
                 if (results[0].discordid != user.id) { return }
                 reaction.message.delete()
             }
+            // if (reaction.emoji.name == '📝') {
+            //     const filter = msg => results[0].discordid == user.id && msg.channel.type == 'DM'
+            //     const collector = reaction.message.channel.createMessageCollector({ filter, time: 120000 })
+            //     let webhooks = await reaction.message.channel.fetchWebhooks()
+            //     let webhook = webhooks.find(wh => wh.owner.id == client.user.id)
+            //     let content = reaction.message.content
+            //     user.send(`Texte actuel : \n\n${content}`)
+
+            //     collector.on('collect', async collected => {
+            //         content = collected.content
+            //         collected.delete()
+            //         collector.stop()
+
+            //         await webhook.editMessage(reaction.message.id, {
+            //             content: content,
+            //         });
+
+            //     })
+            // }
         })
     },
 };
